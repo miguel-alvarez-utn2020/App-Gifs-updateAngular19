0bb4ff918ba25c0248cf857e9cfa86ce2d80f489
Miguel Alvarez <miguel-alvarez-utn2020>
2025-05-03 14:47:59 -0300

    update to angular 19

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  // ApiKeys
+  // https://developers.giphy.com/dashboard/
+  giphyApiKey: 'ICkAVA4iOI3aZyfxoolWhigko43Vkn6f',
+  giphyUrl: 'https://api.giphy.com/v1',
+};
